601761b787f7fe9e4e7802e913564dce3ff308cf
nicholaschun <nicholaschunrayne@gmail.com>
2019-06-08 18:17:09 -0700

    used html template

--- src/frontend/controllers/frontendController.js
+++ src/frontend/controllers/frontendController.js
@@ -1,5 +1,6 @@
+const path = require('path')
 module.exports = {
     indexPage (req, res) {
-        res.render('index')
+        res.sendFile(path.join(__dirname,'./../views/index.html'))
     }
 }
